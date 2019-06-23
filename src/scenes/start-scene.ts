430dd4f681c97a3ad7d5db0907d27c9193db318a
Mgx98 <50836104+Mgx98@users.noreply.github.com>
2019-06-24 00:13:47 +0200

    platform

--- src/scenes/start-scene.ts
+++ src/scenes/start-scene.ts
@@ -14,7 +14,7 @@ export class StartScene extends Phaser.Scene {
         // this.add.image(0, 0, 'item').setOrigin(0, 0)
         this.add.image(0, 0, 'sky')
         // add text here
-        this.add.text(400, 300, 'Rob.ai', { fontFamily: 'Sheeping Cats Straight', fontSize: 70, color: '#000000' }).setOrigin(0.5).setStroke('#2ac9be', 16)
+        this.add.text(400, 300, 'Rob.AI', { fontFamily: 'Sheeping Cats Straight', fontSize: 70, color: '#000000' }).setOrigin(0.5).setStroke('#2ac9be', 16)
 
         // add code here to switch to the GameScene, after a mouse click
         let btn1 = this.add.image(400, 500, 'mybutton1')
